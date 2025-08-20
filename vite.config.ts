45e89beb5adf75e2b2c9557124e23958c3de764b
Andre Ailton <andre.ailtonf.10@gmail.com>
2025-08-20 08:29:14 -0300

    Adicionadohost

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Agenda/', // 👈 importante!
+    server: {
+    allowedHosts: ['andreailtondev.tech'], // libera seu domínio
+    host: true, // aceita conexões externas
+    port: 5174, // porta do dev server
+  },
 })
